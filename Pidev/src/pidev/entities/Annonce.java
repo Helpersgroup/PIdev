e7c07ecf0b6aee6456ec262297e29524b6592e87
Eya <Eya@Eya-PC>
2014-02-21 21:08:46 +0100

    bonjour

--- Pidev/src/pidev/entities/Annonce.java
+++ Pidev/src/pidev/entities/Annonce.java
@@ -9,5 +9,20 @@ package pidev.entities;
  * @author Eya
  */
 public class Annonce {
+    private String nom;
+    private String date_deb;
+    private String date_fin;
+    private String description;
+    private String destination;
+    private String depart;
+    private String hebergement;
+    private String type_hebergement;
+    private String transport;
+    private int    note;
+    private int    nbr_adultes;
+    private int    nbr_enfants;
+    private String theme;
+    
+    
     
 }
