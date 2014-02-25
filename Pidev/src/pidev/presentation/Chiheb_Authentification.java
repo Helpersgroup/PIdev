fca3d85d89413f75c3c534848cfbefb03a69f533
chiheb <chiheb@CHI>
2014-02-25 22:00:16 +0100

    correction authentification client

--- Pidev/src/pidev/presentation/Chiheb_Authentification.java
+++ Pidev/src/pidev/presentation/Chiheb_Authentification.java
@@ -4,9 +4,24 @@
  */
 package pidev.presentation;
 
+import chrriis.dj.nativeswing.swtimpl.NativeInterface;
+import chrriis.dj.nativeswing.swtimpl.components.JWebBrowser;
+import chrriis.dj.nativeswing.swtimpl.components.WebBrowserAdapter;
+import chrriis.dj.nativeswing.swtimpl.components.WebBrowserNavigationEvent;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.StringReader;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.parser.ParserDelegator;
 import pidev.util.MyConnection;
 
 /**
@@ -14,7 +29,24 @@ import pidev.util.MyConnection;
  * @author Abdelaziz
  */
 public class Chiheb_Authentification extends javax.swing.JFrame {
+public static String API_KEY = "224601177742221";
+public static String SECRET = "9d15944887d10081452b7a41f9d01394";
 
+  public static String firstRequest = "https://graph.facebook.com/oauth/authorize?"
+  + "client_id="
+  + API_KEY
+  + "&redirect_uri=http://www.facebook.com/connect/login_success.html&"
+  + "scope=publish_stream,offline_access,create_event,read_stream,email,user_birthday";
+  
+  public static String secondRequest="https://graph.facebook.com/oauth/access_token?"
+  + "client_id="
+  + API_KEY
+  + "&redirect_uri=http://www.facebook.com/connect/login_success.html&"
+  + "client_secret=" + SECRET + "&code=";
+
+  public static String access_token = "";
+  public static boolean firstRequestDone = false;
+  public static boolean secondRequestDone = false;
     /**
      * Creates new form Acceuil
      */
@@ -87,6 +119,11 @@ public class Chiheb_Authentification extends javax.swing.JFrame {
 
         jButton2.setIcon(new javax.swing.ImageIcon("C:\\Users\\chiheb\\Desktop\\facebook_square-128.png")); // NOI18N
         jButton2.setText("jButton2");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
         getContentPane().add(jButton2);
         jButton2.setBounds(160, 220, 60, 70);
 
@@ -200,10 +237,94 @@ public class Chiheb_Authentification extends javax.swing.JFrame {
         this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:7
+
+                SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+                NativeInterface.open();
+                NativeInterface.initialize();
+        final JFrame authFrame = new JFrame();
+    // Create the JWebBrowser and add the WebBrowserAdapter
+    JPanel webBrowserPanel = new JPanel(new BorderLayout());
+    final JWebBrowser webBrowser = new JWebBrowser();
+    webBrowser.setMenuBarVisible(false);
+    webBrowser.setButtonBarVisible(false);
+    webBrowser.setLocationBarVisible(false);
+    webBrowser.navigate(firstRequest);
+    webBrowser.addWebBrowserListener(new WebBrowserAdapter() {
+      @Override
+      public void locationChanged(WebBrowserNavigationEvent e) {
+        super.locationChanged(e);
+        // Check if first request was not done
+        if (!firstRequestDone) {
+          // Check if you left the location and were redirected to the next 
+          // location
+          if (e.getNewResourceLocation().contains("http://www.facebook.com/connect/login_success.html?code=")){
+            // If it successfully redirects you, get the verification code
+            // and go for a second request
+            String[] splits = e.getNewResourceLocation().split("=");
+            String stage2temp = secondRequest + splits[1];
+              System.out.println("First ="+splits[1]);
+             
+              webBrowser.navigate(stage2temp);
+           
+            firstRequestDone = true;
+          }
+        } else {
+          // If secondRequest is not done yet, you perform this and get the 
+          // access_token
+          if (!secondRequestDone) {
+           // System.out.println(webBrowser.getHTMLContent());
+            // Create reader with the html content
+            
+              StringReader readerSTR = new StringReader(webBrowser.getHTMLContent());
+            // Create a callback for html parser
+            HTMLEditorKit.ParserCallback callback = 
+            new HTMLEditorKit.ParserCallback() {
+                @Override
+              public void handleText(char[] data,int pos) {
+                System.out.println(data);
+                // because there is only one line with the access_token 
+                // in the html content you can parse it.
+                String string = new String(data);
+                String[] temp1 = string.split("&");
+                String[] temp2 = temp1[0].split("=");
+                    System.out.println("access tocken="+temp2[1]);
+                access_token = temp2[1];
+              authFrame.dispose();
+                new GraphReaderExample(access_token).runEverything();
+                
+              }
+            };
+            try {
+              // Call the parse method 
+              new ParserDelegator().parse(readerSTR,callback,false);
+            } catch (IOException e1) {
+              e1.printStackTrace();
+            }
+            // After everything is done, you can dispose the jframe
+            //authFrame.dispose();      
+          }
+        }
+      }
+    });
+    webBrowserPanel.add(webBrowser,BorderLayout.CENTER);
+    authFrame.add(webBrowserPanel);
+    authFrame.setSize(550, 400);
+    authFrame.setVisible(true);
+    authFrame.setLocationRelativeTo(null);
+    authFrame.pack();
+    }//GEN-LAST:event_jButton2ActionPerformed
+    
+        });
+    }
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void main(String args[] ) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
---
+++ Pidev/src/pidev/presentation/GraphReaderExample.java
@@ -0,0 +1,265 @@
+package pidev.presentation;
+
+import static java.lang.String.format;
+import static java.lang.System.currentTimeMillis;
+import static java.lang.System.out;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.restfb.Connection;
+import com.restfb.DefaultFacebookClient;
+import com.restfb.DefaultJsonMapper;
+import com.restfb.Facebook;
+import com.restfb.FacebookClient;
+import com.restfb.JsonMapper;
+import com.restfb.Parameter;
+import com.restfb.json.JsonArray;
+import com.restfb.json.JsonObject;
+import com.restfb.types.Page;
+import com.restfb.types.Post;
+import com.restfb.types.Url;
+import com.restfb.types.User;
+import static java.lang.String.format;
+import static java.lang.System.currentTimeMillis;
+import static java.lang.System.out;
+
+@SuppressWarnings("deprecation")
+public class GraphReaderExample {
+  private final FacebookClient facebookClient;
+
+
+  public static void main(String[] args) {
+    new GraphReaderExample("CAADMRglqW40BAHTboMiaZB0SsZAZAZB5edqkEr3ZCIhSRCdEH1671vZBqBVdMQUb3VwOwhF7MfXbGGQ78vdhTx26jTKSrD3K6oDFjkaSnrJ8pCWO9B3pUu08Ai8L7O8ZBZAvXKegUjwxjeXzWxTQZBBtb1VaTeBsVwY9J2Ob0AVkdpTVZAF5aExMMU").runEverything();
+  }
+
+  GraphReaderExample(String accessToken) {
+    facebookClient = new DefaultFacebookClient(accessToken);
+  }
+
+  void runEverything() {
+      fetchObject();
+//    fetchObjects();
+//    fetchObjectsAsJsonObject();
+//    fetchConnections();
+//    fetchDifferentDataTypesAsJsonObject();
+//    query();
+//    multiquery();
+//    search();
+//    metadata();
+//    paging();
+//    selection();
+//    parameters();
+//    rawJsonResponse();
+  }
+
+  void fetchObject() {
+    out.println("* Fetching single objects *");
+
+    User user = facebookClient.fetchObject("me", User.class);
+    Page page = facebookClient.fetchObject("cocacola", Page.class);
+
+    out.println("Name: " + user.getName());
+    out.println("Username: " + user.getUsername());
+    out.println("Email: " + user.getEmail());
+    out.println("ID Facebook: " + user.getId());
+    out.println("About: " + user.getAbout());
+    out.println("Birthday: " + user.getBirthday());
+    
+    //out.println("Page likes: " + page.getLikes());
+  }
+
+  void fetchObjectsAsJsonObject() {
+    out.println("* Fetching multiple objects at once as a JsonObject *");
+
+    List<String> ids = new ArrayList<String>();
+    ids.add("btaylor");
+    ids.add("http://www.imdb.com/title/tt0117500/");
+
+    // Make the API call
+    JsonObject results = facebookClient.fetchObjects(ids, JsonObject.class);
+
+    // Pull out JSON data by key and map each type by hand.
+    JsonMapper jsonMapper = new DefaultJsonMapper();
+    User user = jsonMapper.toJavaObject(results.getString("btaylor"), User.class);
+    Url url = jsonMapper.toJavaObject(results.getString("http://www.imdb.com/title/tt0117500/"), Url.class);
+
+    out.println("User is " + user);
+    out.println("URL is " + url);
+  }
+
+  void fetchObjects() {
+    out.println("* Fetching multiple objects at once *");
+
+    GraphReaderExample.FetchObjectsResults fetchObjectsResults =
+        facebookClient.fetchObjects(Arrays.asList("me", "cocacola"), GraphReaderExample.FetchObjectsResults.class);
+
+    out.println("User name: " + fetchObjectsResults.me.getName());
+    out.println("Page likes: " + fetchObjectsResults.page.getLikes());
+  }
+
+  void fetchDifferentDataTypesAsJsonObject() {
+    out.println("* Fetching different types of data as JsonObject *");
+
+    JsonObject btaylor = facebookClient.fetchObject("btaylor", JsonObject.class);
+    out.println(btaylor.getString("name"));
+
+    JsonObject photosConnection = facebookClient.fetchObject("me/photos", JsonObject.class);
+    JsonArray photosConnectionData = photosConnection.getJsonArray("data");
+
+    if (photosConnectionData.length() > 0) {
+      String firstPhotoUrl = photosConnectionData.getJsonObject(0).getString("source");
+      out.println(firstPhotoUrl);
+    }
+
+    String query = "SELECT uid, name FROM user WHERE uid=220439 or uid=7901103";
+    List<JsonObject> queryResults = facebookClient.executeQuery(query, JsonObject.class);
+
+    if (queryResults.size() > 0)
+      out.println(queryResults.get(0).getString("name"));
+  }
+  
+  public static class FetchObjectsResults {
+    @Facebook
+    User me;
+
+    @Facebook(value = "cocacola")
+    Page page;
+  }
+
+  void fetchConnections() {
+    out.println("* Fetching connections *");
+
+    Connection<User> myFriends = facebookClient.fetchConnection("me/friends", User.class);
+    Connection<Post> myFeed = facebookClient.fetchConnection("me/feed", Post.class);
+
+    out.println("Count of my friends: " + myFriends.getData().size());
+
+    if (myFeed.getData().size() > 0)
+      out.println("First item in my feed: " + myFeed.getData().get(0).getMessage());
+  }
+
+  void query() {
+    out.println("* FQL Query *");
+
+    List<GraphReaderExample.FqlUser> users =
+        facebookClient.executeQuery("SELECT uid, name FROM user WHERE uid=220439 or uid=7901103", GraphReaderExample.FqlUser.class);
+
+    out.println("User: " + users);
+  }
+
+  void multiquery() {
+    out.println("* FQL Multiquery *");
+
+    Map<String, String> queries = new HashMap<String, String>();
+    queries.put("users", "SELECT uid, name FROM user WHERE uid=220439 OR uid=7901103");
+    queries.put("likers", "SELECT user_id FROM like WHERE object_id=122788341354");
+
+    GraphReaderExample.MultiqueryResults multiqueryResults = facebookClient.executeMultiquery(queries, GraphReaderExample.MultiqueryResults.class);
+
+    out.println("Users: " + multiqueryResults.users);
+    out.println("People who liked: " + multiqueryResults.likers);
+  }
+
+  public static class FqlUser {
+    @Facebook
+    String uid;
+
+    @Facebook
+    String name;
+
+    @Override
+    public String toString() {
+      return format("%s (%s)", name, uid);
+    }
+  }
+
+  public static class FqlLiker {
+    @Facebook("user_id")
+    String userId;
+
+    @Override
+    public String toString() {
+      return userId;
+    }
+  }
+
+  /**
+   * Holds results from a "multiquery" call.
+   */
+  public static class MultiqueryResults {
+    @Facebook
+    List<GraphReaderExample.FqlUser> users;
+
+    @Facebook
+    List<GraphReaderExample.FqlLiker> likers;
+  }
+
+  void search() {
+    out.println("* Searching connections *");
+
+    Connection<Post> publicSearch =
+        facebookClient.fetchConnection("search", Post.class, Parameter.with("q", "ben foulen"),
+          Parameter.with("type", "post"));
+
+    Connection<User> targetedSearch =
+        facebookClient.fetchConnection("me/home", User.class, Parameter.with("q", "foulen"),
+          Parameter.with("type", "user"));
+
+    if (publicSearch.getData().size() > 0)
+      out.println("Public search: " + publicSearch.getData().get(0).getMessage());
+
+    out.println("Posts on my wall by friends named Mark: " + targetedSearch.getData().size());
+  }
+
+  void metadata() {
+    out.println("* Metadata *");
+
+    User userWithMetadata = facebookClient.fetchObject("me", User.class, Parameter.with("metadata", 1));
+
+    out.println("User metadata: has albums? " + userWithMetadata.getMetadata().getConnections().hasAlbums());
+  }
+
+  void paging() {
+    out.println("* Paging support *");
+
+    Connection<User> myFriends = facebookClient.fetchConnection("me/friends", User.class);
+    Connection<Post> myFeed = facebookClient.fetchConnection("me/feed", Post.class, Parameter.with("limit", 100));
+
+    out.println("Count of my friends: " + myFriends.getData().size());
+
+    if (myFeed.getData().size() > 0)
+      out.println("First item in my feed: " + myFeed.getData().get(0));
+
+    for (List<Post> myFeedConnectionPage : myFeed)
+      for (Post post : myFeedConnectionPage)
+        out.println("Post from my feed: " + post);
+  }
+
+  void selection() {
+    out.println("* Selecting specific fields *");
+
+    User user = facebookClient.fetchObject("me", User.class, Parameter.with("fields", "id,name"));
+
+    out.println("User name: " + user.getName());
+  }
+
+  void parameters() {
+    out.println("* Parameter support *");
+
+    Date oneWeekAgo = new Date(currentTimeMillis() - 1000L * 60L * 60L * 24L * 7L);
+
+    Connection<Post> filteredFeed =
+        facebookClient.fetchConnection("me/feed", Post.class, Parameter.with("limit", 3),
+          Parameter.with("until", "yesterday"), Parameter.with("since", oneWeekAgo));
+
+    out.println("Filtered feed count: " + filteredFeed.getData().size());
+  }
+
+  void rawJsonResponse() {
+    out.println("* Raw JSON *");
+    out.println("User object JSON: " + facebookClient.fetchObject("me", String.class));
+  }
+}
