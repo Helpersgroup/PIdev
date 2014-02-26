38885092ce0e15e273c02bcd807543c72a44901c
unknown <unknown@unknown-PC>
2014-02-26 01:42:23 +0100

    interface evaluation2

--- Pidev/src/pidev/dao/EvalDAO.java
+++ Pidev/src/pidev/dao/EvalDAO.java
@@ -16,17 +16,12 @@ import java.util.logging.Logger;
 import pidev.util.MyConnection;
 
 
-/**
- *
- * @author unknown
- */
+
 public class EvalDAO {
     
 public static   EvalDAO   evalDAO=null;
     
-
-
-    public void insertCommentaire(String msg,int id_Personne,int id_Annonce){
+    public void AjouterCommentaire(String msg,int id_Personne,int id_Annonce){
 
          String requete = "insert into Commentaire (id_Annonce,id_Personne,message) values (?,?,?)";
         try { 
@@ -41,5 +36,59 @@ public static   EvalDAO   evalDAO=null;
             System.out.println("erreur lors de l'insertion "+ex.getMessage());
         }
     }
+    
+    public void SupprimerCom(int id){
 
+          String requete = "delete from message where id=?";
+        try {
+            PreparedStatement ps = MyConnection.getInstance().prepareStatement(requete);
+            ps.setInt(1, id);
+            ps.executeUpdate();
+            System.out.println("Suppression effectuée avec succès");
+        } catch (SQLException ex) {
+           //Logger.getLogger(PersonneDao.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("erreur lors de la suppression "+ex.getMessage());
+        }
+    }
+public void jaime(int id_Personne,int id_Annonce){
+
+         String requete = "insert into Jaime (id_Annonce,id_Personne) values (?,?)";
+        try { 
+            PreparedStatement ps = MyConnection.getInstance().prepareStatement(requete);
+            ps.setInt(1, id_Annonce);
+            ps.setInt(2, id_Personne);
+            ps.executeUpdate();
+            System.out.println("Jaime c bon");
+        } catch (SQLException ex) {
+           
+            System.out.println("erreur lors de l'insertion "+ex.getMessage());
+        }
+    }
+
+public void note(int n,int id_Annonce){
+            
+         String req = "select note from Annnonce where id_annonce="+id_Annonce;
+         Statement statement;
+        int r=0;
+         try {
+            statement = MyConnection.getInstance().createStatement();
+            ResultSet resultat = statement.executeQuery(req);
+           
+        r = (n+resultat.getInt(2))/2;
+        } catch (SQLException ex) {
+            Logger.getLogger(EvalDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+               
+         String requete = "update Annonce set note = ? where id_Annonce="+id_Annonce;
+        
+         try { 
+            PreparedStatement ps = MyConnection.getInstance().prepareStatement(requete);
+            ps.setInt(1,r );
+            ps.executeUpdate();
+            System.out.println("note c bon");
+        } catch (SQLException ex) {
+           
+            System.out.println("erreur lors de l'insertion "+ex.getMessage());
+        }
+    }
 }
